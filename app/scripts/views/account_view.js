40e40b1fabd5e6fd1ecc6f00b574ce56a4f9ba4c
Diana Fisher <diana.fisher@gmail.com>
2016-02-16 14:43:19 -0800

    Add password validation.

--- app/scripts/views/account_view.js
+++ app/scripts/views/account_view.js
@@ -11,14 +11,9 @@ app.AccountView = Backbone.View.extend({
     className: 'accont-view',
 
     // The DOM events specific to an item.
-    // events: {
-    //     'click .toggle': 'toggleCompleted',
-    //     'dblclick label': 'edit',
-    //     'click .destroy': 'clear',
-    //     'keypress .edit': 'updateOnEnter',
-    //     'keydown .edit': 'revertOnEscape',
-    //     'blur .edit': 'close'
-    // },
+    events: {
+        'keyup #inputPassword': 'validatePassword',
+    },
 
     ENTER_KEY: 13,
     ESCAPE_KEY: 27,
@@ -28,6 +23,7 @@ app.AccountView = Backbone.View.extend({
     // loading any preexisting todos that might be saved in *localStorage*.
     initialize: function () {
         console.log('account view initialize');
+
         // this.$account = this.$('#account');
 
         // this.allCheckbox = this.$('#toggle-all')[0];
@@ -35,20 +31,70 @@ app.AccountView = Backbone.View.extend({
         // this.$footer = this.$('#footer');
         // this.$main = this.$('#main');
         // this.$todoList = this.$('#todo-list');
-
-        // this.listenTo(this.collection, 'add', this.addOne);
-        // this.listenTo(this.collection, 'reset', this.addAll);
-        // this.listenTo(this.collection, 'change:completed', this.filterOne);
-        // this.listenTo(this.collection, 'filter', this.filterAll);
-        // this.listenTo(this.collection, 'all', this.render);
-
-        // this.collection.fetch();
     },
 
-    render: function () { 
-        // this.$el.html(this.template());       
+    render: function () {         
         console.log('render AccountView');
         this.$el.html(this.template());
+        this.$inputPassword = this.$('#inputPassword');
+        this.$repeatPassword = this.$('#repeatPassword');
+        
         return this;
     },
+
+    validatePassword: function(e) {
+        var password = this.$inputPassword.val().trim();
+        console.log(password);
+        this.validateLength(password);
+        this.validateSymbol(password);
+        this.validateNumber(password);
+        this.validateLower(password);
+        this.validateUpper(password);
+    },
+
+    validateLength: function(password) {
+        console.log(password.length);
+        if (password.length >= 16) {
+            this.$('#pwd-length').removeClass('bg-danger');
+            this.$('#pwd-length').addClass('bg-success');
+        } else {
+           this.$('#pwd-length').addClass('bg-danger');
+        }
+    },
+
+    validateSymbol: function(password) {
+        if (password.match(/[\!\@\#\$\%\^\*]/g)) {
+           this.$('#pwd-symbol').removeClass('bg-danger');
+            this.$('#pwd-symbol').addClass('bg-success');
+        } else {
+           this.$('#pwd-symbol').addClass('bg-danger');
+        }
+    },
+
+    validateNumber: function(password) {
+        if (password.match(/[0-9]/g)) {
+           this.$('#pwd-number').removeClass('bg-danger');
+            this.$('#pwd-number').addClass('bg-success');
+        } else {
+           this.$('#pwd-number').addClass('bg-danger');
+        }
+    },
+
+    validateLower: function(password) {
+       if (password.match(/[a-z]/g)) {
+           this.$('#pwd-lower').removeClass('bg-danger');
+            this.$('#pwd-lower').addClass('bg-success');
+        } else {
+           this.$('#pwd-lower').addClass('bg-danger');
+        }
+    },
+
+    validateUpper: function(password) {
+       if (password.match(/[A-Z]/g)) {
+           this.$('#pwd-upper').removeClass('bg-danger');
+            this.$('#pwd-upper').addClass('bg-success');
+        } else {
+           this.$('#pwd-upper').addClass('bg-danger');
+        }
+    }
 });
