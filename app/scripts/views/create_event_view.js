6c6cd539c083d7f4108296f93a3f45cd76b9bc93
Diana Fisher <diana.fisher@gmail.com>
2016-02-17 17:16:35 -0800

    Add Google Places API to create event form.

--- app/scripts/views/create_event_view.js
+++ app/scripts/views/create_event_view.js
@@ -11,16 +11,17 @@ app.CreateEventView = Backbone.View.extend({
     className: 'create-event-view',
 
     events: {
+        'focus #inputLocation': 'geolocate',
         'submit': 'createEvent'
     },
 
     initialize: function () {
-        console.log('login view initialize');        
+        console.log('login view initialize');
     },
 
     render: function () {
         console.log('render CreateEventView');
-        this.$el.html(this.template());       
+        this.$el.html(this.template());
         
         return this;
     },
@@ -33,3 +34,68 @@ app.CreateEventView = Backbone.View.extend({
         app.router.navigate('#', {trigger: true});
     }
 });
+
+var placeSearch, autocomplete;
+var componentForm = {
+  street_number: 'short_name',
+  route: 'long_name',
+  locality: 'long_name',
+  administrative_area_level_1: 'short_name',
+  country: 'long_name',
+  postal_code: 'short_name'
+};
+
+
+function initAutocomplete() {
+    // Create the autocomplete object, restricting the search to geographical
+    // location types.
+    autocomplete = new google.maps.places.Autocomplete(
+    /** @type {!HTMLInputElement} */(document.getElementById('inputLocation')),
+    {types: ['geocode']});
+
+    // When the user selects an address from the dropdown, populate the address
+    // fields in the form.
+    autocomplete.addListener('place_changed', fillInAddress);
+}
+
+// [START region_fillform]
+function fillInAddress() {
+  // Get the place details from the autocomplete object.
+  var place = autocomplete.getPlace();
+
+  for (var component in componentForm) {
+    document.getElementById(component).value = '';
+    document.getElementById(component).disabled = false;
+  }
+
+  // Get each component of the address from the place details
+  // and fill the corresponding field on the form.
+  for (var i = 0; i < place.address_components.length; i++) {
+    var addressType = place.address_components[i].types[0];
+    if (componentForm[addressType]) {
+      var val = place.address_components[i][componentForm[addressType]];
+      document.getElementById(addressType).value = val;
+    }
+  }
+}
+// [END region_fillform]
+
+// [START region_geolocation]
+// Bias the autocomplete object to the user's geographical location,
+// as supplied by the browser's 'navigator.geolocation' object.
+function geolocate() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var geolocation = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      var circle = new google.maps.Circle({
+        center: geolocation,
+        radius: position.coords.accuracy
+      });
+      autocomplete.setBounds(circle.getBounds());
+    });
+  }
+}
+// [END region_geolocation]
